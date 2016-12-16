0cc4c20f87fca9cf10d727912eaa3a7347ba9b9a
Jonathan Thom <jonathan.thom1990@gmail.com>
2016-12-16 08:33:51 -0800

    pass integration spec for no empty brand name

--- spec/brand_integration_spec.rb
+++ spec/brand_integration_spec.rb
@@ -7,4 +7,11 @@ describe('The brand route', {:type => :feature}) do
     click_button('Add Brand')
     expect(page).to have_content('Nike')
   end
+
+  it('will not allow users to enter a blank brand name') do
+    visit('/')
+    fill_in('brand_name', :with => '')
+    click_button('Add Brand')
+    expect(page).to have_content('Name can\'t be blank')
+  end
 end
