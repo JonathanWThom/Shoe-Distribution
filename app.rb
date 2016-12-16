2dcab394df8d9ff45d643817474382c7fb563a71
Jonathan Thom <jonathan.thom1990@gmail.com>
2016-12-15 19:03:34 -0800

    add a new store route

--- app.rb
+++ app.rb
@@ -3,3 +3,17 @@ Bundler.require(:default)
 require('pry')
 
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+
+get('/') do
+  erb(:index)
+end
+
+post('/stores') do
+  @store = Store.create(:name => params['store_name'])
+  redirect('/stores/'.concat(@store.id().to_s()))
+end
+
+get('/stores/:id') do
+  @store = Store.find(params['id'].to_i)
+  erb(:store)
+end
