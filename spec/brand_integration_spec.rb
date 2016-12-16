9550a13d32bc394be22a059e093021417ce35146
Jonathan Thom <jonathan.thom1990@gmail.com>
2016-12-16 08:31:58 -0800

    pass integration spec for adding brand

--- spec/brand_integration_spec.rb
+++ spec/brand_integration_spec.rb
@@ -1,1 +1,10 @@
 require('spec_helper')
+
+describe('The brand route', {:type => :feature}) do
+  it('will add a new brand') do
+    visit('/')
+    fill_in('brand_name', :with => 'nike')
+    click_button('Add Brand')
+    expect(page).to have_content('Nike')
+  end
+end
