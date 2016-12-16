f32f6f98a0ea738ae207a195103a22f6da395964
Jonathan Thom <jonathan.thom1990@gmail.com>
2016-12-16 08:18:20 -0800

    pass add shoe store integration spec

--- spec/store_integration_spec.rb
+++ spec/store_integration_spec.rb
@@ -1,1 +1,10 @@
 require('spec_helper')
+
+describe('The shoe store route', {:type => :feature}) do
+  it('will add a new shoe store') do
+    visit('/')
+    fill_in('store_name', :with => 'payless shoestore')
+    click_button('Add Store')
+    expect(page).to have_content('Payless Shoestore')
+  end
+end
