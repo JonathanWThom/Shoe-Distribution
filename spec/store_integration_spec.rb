ea2c47f6b1f1432862d617c507a9fe8c20d25449
Jonathan Thom <jonathan.thom1990@gmail.com>
2016-12-16 08:28:38 -0800

    pass integration spec for adding brand to store

--- spec/store_integration_spec.rb
+++ spec/store_integration_spec.rb
@@ -22,4 +22,13 @@ describe('The shoe store route', {:type => :feature}) do
     click_button('Change Name')
     expect(page).to have_content('Costco')
   end
+
+  it('will add a brand to a shoe store') do
+    store = Store.create(:name => 'outlet mall')
+    brand = Brand.create(:name => 'nike')
+    visit("/stores/#{store.id()}")
+    check('Nike')
+    click_button('Add Brand')
+    expect(page).to have_content('Nike')
+  end
 end
