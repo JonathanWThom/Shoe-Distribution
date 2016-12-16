42259cd6f9446fc27ab4de96425f33dc0f5950db
Jonathan Thom <jonathan.thom1990@gmail.com>
2016-12-15 17:26:24 -0800

    Add titleize method for brand

--- lib/brand.rb
+++ lib/brand.rb
@@ -1,3 +1,10 @@
 class Brand < ActiveRecord::Base
   has_and_belongs_to_many(:stores)
+  before_save(:titleize)
+
+  private
+
+  define_method(:titleize) do
+    self.name=(name().titleize())
+  end
 end
--- spec/brand_spec.rb
+++ spec/brand_spec.rb
@@ -2,4 +2,9 @@ require('spec_helper')
 
 describe(Brand) do
   it { should have_and_belong_to_many(:stores)}
+
+  it('capitalizes all words in a brand name') do
+    test_brand = Brand.create({:name => 'new balance'})
+    expect(test_brand.name()).to(eq('New Balance'))
+  end
 end
