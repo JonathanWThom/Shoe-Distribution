ae8e86d985f165fe1eb6e023654f45e595f62f0d
Jonathan Thom <jonathan.thom1990@gmail.com>
2016-12-15 17:29:12 -0800

    Add titleize method for store

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,10 @@
 class Store < ActiveRecord::Base
   has_and_belongs_to_many(:brands)
+  before_save(:titleize)
+
+  private
+
+  define_method(:titleize) do
+    self.name=(name().titleize())
+  end
 end
--- spec/store_spec.rb
+++ spec/store_spec.rb
@@ -2,4 +2,9 @@ require('spec_helper')
 
 describe(Store) do
   it { should have_and_belong_to_many(:brands)}
+
+  it('capitalizes all words in a store name') do
+    test_store = Store.create({:name => 'famous footwear'})
+    expect(test_store.name()).to(eq('Famous Footwear'))
+  end
 end
